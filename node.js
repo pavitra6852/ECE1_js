855691abda22e17c39b9e4a63e4c1784295b9631
pavitra verma <pavitra6852@gmail.com>
2026-02-27 23:06:32 +0530

    push

---
+++ node.js
@@ -0,0 +1,36 @@
+// var num = 10;
+// console.log(num)
+// console.log(typeof(num))
+//  num ="hello world";
+//  console.log(num)
+// console.log(typeof(num))
+// num = true;
+// console.log(num) 
+// console.log(typeof(num))
+// num = null;
+// console.log(num)
+// console.log(typeof(num))
+// num = undefined;
+// console.log(num)
+// console.log(typeof(num))
+
+const sym1 = Symbol(4);
+console.log(sym1)
+const sym2 = Symbol(4);
+console.log(sym2)
+if(sym1 === sym2){
+    console.log(ture)
+}
+
+
+
+//undefined==null(true)but undefined===null(false)
+// 1 present in working directory
+// 2 git add .
+// ternary operator - (condition) ? expr1 : expr2
+// maximum of 3 numbers
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// let max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+// console.log(max)
